e524fe5b3160037dd68c16ed2c18bbd52f3287c6
Adam Giegel <adam.giegel@gmail.com>
2018-09-24 20:36:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 function dwarfRollCall(dwarves) {
+  let string = ""
+  for(let i = 0; i < dwarves.length; i++){
+    string += ((i + 1) + '.' + " " + dwarves[i] + " ")
+  }
+  return string
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  let newArr = []
+  for(let i = 0; i < planeteerCalls.length; i++){
+    newArr.push(planeteerCalls[i].toUpperCase + "!")
+  }
+  return newArr
 }
 
 function longPlaneteerCalls(words) {
+  for(let i = 0; i < words.length; i++){
+    return words[i].length > 4
+  }
 }
 
 function findTheCheese (foods) {
+  let string = ''
+  for(let i = 0; i < foods.length; i++){
+    if(foods[i] === "cheddar" || foods[i] === "gouda" || foods[i]=== "camembert"){
+      return string += foods[i]
+    }
+  }
+  return "no cheese!"
 }
